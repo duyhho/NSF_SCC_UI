d555bb4ee6526dce8dca8a4378f3d4da626e4bbe
Peter Trinh <peter@particlespace.com>
2020-12-03 02:12:11 -0600

    Finalize pop up and address

--- src/components/Map/Map311.jsx
+++ src/components/Map/Map311.jsx
@@ -1,25 +1,30 @@
 import React, { Component } from "react"
-import { Map, Marker, GoogleApiWrapper, } from "google-maps-react"
+import { Map, Marker, GoogleApiWrapper, InfoWindow } from "google-maps-react"
 import ImageGallery from 'react-image-gallery'
 import update from 'immutability-helper'
 import axios from 'axios'
 
 import { modal } from '../../utilities/modal.js'
 import ProgressBar from '../ProgressBar/ProgressBar.jsx'
+import { FilterTiltShiftSharp } from "@material-ui/icons"
 
 export class Map311 extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            serverDomain: "https://502c177ca878.ngrok.io",
-            coordinatesList: [{case_id: 0, lat: 39.0410436302915, lng: -94.5876739197085}],
+            serverDomain: "https://51a14464797b.ngrok.io",
+            processedData: [],
             firstImageReturned: false,
             imageList: [],
             returnedPercent: 0,
             serverError: true,
             rawData: null,
-
+            infoWindowContent: (
+                <div></div>
+            ),
+            showingInfoWindow: false,
+            activeMarker: {},
         };
     }
     
@@ -32,21 +37,9 @@ export class Map311 extends Component {
 
         axios.get(this.state.serverDomain + "/api/311/get/all")
         .then(function(response) {
-            var jsonData = JSON.parse(response.data)
+            console.log(response.data)
             self.setState({
-                rawData: jsonData
-            })
-
-            jsonData.forEach(function(request) {
-                console.log(request)
-                self.setState({
-                    coordinatesList: update(self.state.coordinatesList, {$push: [{
-                        case_id: request.case_id,
-                        lat: request.lat,
-                        lng: request.lng
-                    }]
-                    }),
-                })
+                processedData: response.data
             })
         })
         .catch(function(error) {
@@ -58,18 +51,39 @@ export class Map311 extends Component {
         })
     }
 
-    onMarkerClick() {
-        this.setState({
-            imageList: [],
-            dataLoading: true,
-            returnedPercent: 0,
+    onMarkerClick(props, marker, e) {
+        var self = this;
+        const processData = this.state.processedData;
+
+        processData.forEach(function(location) {
+            if (location.case_id == props.caseId) {
+                self.setState({
+                    activeMarker: marker,
+                    showingInfoWindow: true,
+                    infoWindowContent: (
+                    <div>
+                        <h2>Address</h2>
+                        <b>{location.address + ", Kansas City, MO " + location.zip_code}</b>
+                    </div>
+                    )
+                });
+            }
         })
 
         //Set firstImageReturned = true + imageList
     }
 
+    onMapClicked(mapProps, map, clickEvent) { 
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null,
+            })
+        }
+    };
+
     render() {
-        const coordinatesList = this.state.coordinatesList;
+        const processedData = this.state.processedData;
         const imageList = this.state.imageList;
         const firstImageReturned = this.state.firstImageReturned;
         const returnedPercent = this.state.returnedPercent;
@@ -83,63 +97,75 @@ export class Map311 extends Component {
         
         return (
         <div>
-            <div className="row">
-                <div className="col-md-6 map-view-container">
-                    <div className="map-container">
-                        <Map
-                            google={this.props.google} 
-                            initialCenter={{lat: coordinatesList[0].lat, lng: coordinatesList[0].lng}}
-                            center={{lat: coordinatesList[0].lat, lng: coordinatesList[0].lng}}
-                            zoom={14}
-                        >
-                        
-                        {coordinatesList.map((location) =>
-                        <Marker
-                            position={{lat: location.lat, lng: location.lng}}
-                            name={"311 Location"}
-                            icon={{
-                                //TODO: Move image to local
-                              url: "https://p7.hiclipart.com/preview/1020/199/663/computer-icons-clip-art-green-circle-icon.jpg",
-                              anchor: new window.google.maps.Point(64, 64),
-                              scaledSize: new window.google.maps.Size(15, 15)
-                            }}
-                            onClick={this.onMarkerClick.bind(this)}
-                        />
-                        )}
+            {processedData.length > 0 && (
+            <div>
+                <div className="row">
+                    <div className="col-md-6 map-view-container">
+                        <div className="map-container">
+                            <Map
+                                google={this.props.google} 
+                                initialCenter={{lat: processedData[0].lat, lng: processedData[0].lng}}
+                                center={{lat: processedData[0].lat, lng: processedData[0].lng}}
+                                zoom={14}
+                                onClick={this.onMapClicked.bind(this)}
+                            >
+                            
+                            {processedData.map((location) =>
+                            <Marker
+                                position={{lat: location.lat, lng: location.lng}}
+                                caseId={location.case_id}
+                                icon={{
+                                    //TODO: Move image to local
+                                url: "https://p7.hiclipart.com/preview/1020/199/663/computer-icons-clip-art-green-circle-icon.jpg",
+                                anchor: new window.google.maps.Point(64, 64),
+                                scaledSize: new window.google.maps.Size(10, 10)
+                                }}
+                                onClick={this.onMarkerClick.bind(this)}
+                            />
+                            )}
 
-                        </Map>
+                            <InfoWindow
+                                marker={this.state.activeMarker}
+                                visible={this.state.showingInfoWindow}
+                            >
+                                {this.state.infoWindowContent}
+                            </InfoWindow>
+
+                            </Map>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-5" align="center">
-                    {imageList.length > 0 ? (
-                    <div>
-                        <ImageGallery
-                            items={imageList}
-                            showPlayButton={false}
-                        />
-                    </div>
-                    ) : (
-                    <div>
-                        {firstImageReturned === false && (
+                    <div className="col-md-5" align="center">
+                        {imageList.length > 0 ? (
                         <div>
-                            {helpText}
+                            <ImageGallery
+                                items={imageList}
+                                showPlayButton={false}
+                            />
+                        </div>
+                        ) : (
+                        <div>
+                            {firstImageReturned === false && (
+                            <div>
+                                {helpText}
+                            </div>
+                            )}
                         </div>
                         )}
+                        {(firstImageReturned === true && serverError === false) && (
+                            <div>
+                                <br />
+                                <ProgressBar bgcolor={"#00695c"} completed={returnedPercent} />
+                            </div>
+                        )}
                     </div>
-                    )}
-                    {(firstImageReturned === true && serverError === false) && (
-                        <div>
-                            <br />
-                            <ProgressBar bgcolor={"#00695c"} completed={returnedPercent} />
-                        </div>
-                    )}
+                </div>
+                <div className="row">
+                    <div className="col-md-6" align="center">
+                        GOOGLE STREET VIEW 3D HERE!
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-6" align="center">
-                    GOOGLE STREET VIEW 3D HERE!
-                </div>
-            </div>
+            )}
         </div>
         );
     }
