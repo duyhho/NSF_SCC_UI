90dd74f5c520f8f029167fdcbc105b5044259a11
benamreview <dhh3hb@mail.umkc.edu>
2020-12-06 22:25:49 -0600

    Update MapStreetView.jsx

--- src/components/Map/MapStreetView.jsx
+++ src/components/Map/MapStreetView.jsx
@@ -43,7 +43,7 @@ export class MapStreetView extends Component {
       firstLoad: true,
       firstImageReturned: false,
       returnedPercent: 0,
-      serverDomain: server.getServerDomain(),
+      serverDomain: 'https://a2361fd1c157.ngrok.io',
       // vrViewUrl: "https://f0c06ecb4442.ngrok.io",
       vrView: null,
       neighborhoodList: [],
