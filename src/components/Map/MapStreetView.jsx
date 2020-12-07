8a6f288972b0e92735d5bd7c860615389e4e20c3
benamreview <dhh3hb@mail.umkc.edu>
2020-12-06 23:05:37 -0600

    Update MapStreetView.jsx

--- src/components/Map/MapStreetView.jsx
+++ src/components/Map/MapStreetView.jsx
@@ -228,7 +228,7 @@ export class MapStreetView extends Component {
     // if (serverDomain.search('https') === -1){
     //   serverDomain = serverDomain.replace("http", 'https')
     // }
-    var URL = "https://a2361fd1c157.ngrok.io" + "/api/GSV/stream?category=" + category + '&start_coord=' + start_coord + '&end_coord=' + end_coord
+    var URL = "https://a2361fd1c157.ngrok.io/api/GSV/stream?category=" + category + '&start_coord=' + start_coord + '&end_coord=' + end_coord
     // URL = 'https://a2361fd1c157.ngrok.io/api/GSV/stream/?category=vehicle&start_coord={%22lat%22:39.091172069883214,%22lng%22:-94.56332980256805}&end_coord={%22lat%22:39.077120436304114,%22lng%22:-94.55237284327451}'
     console.log('calling ' + URL)
 
