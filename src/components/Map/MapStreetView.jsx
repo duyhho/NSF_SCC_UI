f6af46a027f2254cb33bd728b1df5ad8a8202757
Peter Trinh <peter@particlespace.com>
2020-12-07 00:10:09 -0600

    Revert "Update MapStreetView.jsx"
    This reverts commit f307978e7748748a05ae8ded880f73a71b6bc361.

--- src/components/Map/MapStreetView.jsx
+++ src/components/Map/MapStreetView.jsx
@@ -222,15 +222,13 @@ export class MapStreetView extends Component {
 
     var self = this;
     // const start_coord = JSON.stringify(this.state.fields.start_location)
-    // const end_coord = JSON.stringify(this.state.fields.end_location)
+    const end_coord = JSON.stringify(this.state.fields.end_location)
     // const category = this.state.category;
     // var serverDomain = this.state.serverDomain;
     // if (serverDomain.search('https') === -1){
     //   serverDomain = serverDomain.replace("http", 'https')
     // }
-    var URL = "https://a2361fd1c157.ngrok.io/api/GSV/stream?category=utility"
-    // var URL = "https://a2361fd1c157.ngrok.io/api/GSV/stream?category=utility&start_coord={%22lat%22:39.091172069883214,%22lng%22:-94.56332980256805}&end_coord=" + end_coord
-
+    var URL = "https://a2361fd1c157.ngrok.io/api/GSV/stream?category=utility&start_coord={%22lat%22:39.091172069883214,%22lng%22:-94.56332980256805}&end_coord=" + end_coord
     // URL = 'https://a2361fd1c157.ngrok.io/api/GSV/stream/?category=vehicle&start_coord={%22lat%22:39.091172069883214,%22lng%22:-94.56332980256805}&end_coord={%22lat%22:39.077120436304114,%22lng%22:-94.55237284327451}'
     console.log('calling ' + URL)
 
