2ee6584641f0aa7ddaec93387da5376261b7b28d
Peter Trinh <peter@particlespace.com>
2020-12-07 00:10:16 -0600

    Revert "Update MapStreetView.jsx"
    This reverts commit 6e3825e1e89c95afc6716189a37329279628b122.

--- src/components/Map/MapStreetView.jsx
+++ src/components/Map/MapStreetView.jsx
@@ -263,7 +263,6 @@ export class MapStreetView extends Component {
     }
 
     eventSource.onerror = e => {
-      console.log(e)
       console.log('error at: ' + URL)
 
       modal.showInfo("Error while connecting with the server!", "danger", "top", "center");
